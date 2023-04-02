424d5c3fb9ce5317b9a7958a43df437083bf0cb1
wuquanquan <1119594587>
2023-04-02 18:55:42 +0800

    文件切片上传

---
+++ 文件伤处/文件切片上传.js
@@ -0,0 +1,16 @@
+function slice(file, piece = 1024 * 1024 * 5) {
+    let totalSize = file.size; // 文件总大小
+    let start = 0; // 每次上传的开始字节
+    let end = start + piece; // 每次上传的结尾字节
+    let chunks = []
+    while (start < totalSize) {
+      // 根据长度截取每次需要上传的数据
+      // File对象继承自Blob对象，因此包含slice方法
+      let blob = file.slice(start, end); 
+      chunks.push(blob)
+  
+      start = end;
+      end = start + piece;
+    }
+    return chunks
+  }
