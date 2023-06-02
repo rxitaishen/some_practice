6c6a35bbde3132c18baa64cabd3ce2087678b02f
wuquanquan <1119594587>
2023-06-02 09:34:51 +0800

    练习提交

--- 百度面试/并发请求.js
+++ 百度面试/并发请求.js
@@ -1,60 +1,52 @@
 // https://juejin.cn/post/7081150028529532964
 // 假定全局声明ajax，写一个并发处理的request，
 // 要求能并发发送不超过pool个请求
-function createRequest(config) {
-    const { pool } = config;
-    queue = []; // 待处理的队列
-    processCount = 0; // 处理中的个数
-
-    function processNext() {
-        if (processCount >= pool) {
-            // 超过并发数
-            return; 
-        }
-        const config = queue.shift();
-        if (config) {
-            processCount ++;
-            const p = ajax(config.url, config.params);
-            p.finally(res => {
-                // 请求完成，不管有没有成功都继续
-                processCount --;
-                // 执行下一个
-                processNext();
-            });
-            // 
-            p.then(config.resolve, config.reject);
-        }
+async function fun1(){
+    if (!file) return
+    // 创建切片   
+    // let size = 1024 * 1024 * 10; //10MB 切片大小
+    let size = 1024 * 50 //50KB 切片大小
+    let fileChunks = []
+    let index = 0 //切片序号
+    for(let cur = 0; cur < file.size; cur += size){
+        fileChunks.push({
+            hash: index++,
+            chunk: file.slice(cur, cur + size)
+        });
     }
-
-    return function(url, params) {
-        return new Promise((resolve, reject) => {
-            queue.push({
-                url, params, resolve, reject
-            });
-            processNext();
+    // 控制并发
+    let pool = []//并发池
+    let max = 3 //最大并发量
+    for(let i=0;i<fileChunks.length;i++){
+        let item = fileChunks[i]
+        let formData = new FormData()
+        formData.append('filename', file.name)
+        formData.append('hash', item.hash)
+        formData.append('chunk', item.chunk)
+        // 上传切片
+        let task = axios({
+            method: 'post',
+            url: '/upload',
+            data: formData
         })
+        task.then((data)=>{
+            //请求结束后将该Promise任务从并发池中移除
+            let index = pool.findIndex(t=> t===task)
+            pool.splice(index)
+        })
+        pool.push(task)
+        if(pool.length === max){
+            //每当并发池跑完一个任务，就再塞入一个任务
+            await Promise.race(pool) // 结合上面的task.then(),这里resolve了什么的task也会自己删一个。
+        }
     }
-
+    //所有任务完成,合并切片
+    await axios({
+        method: 'get',
+        url: '/merge',
+        params: {
+            filename: file.name
+        }
+    });
+    console.log('上传完成')
 }
-
-// chatgpt对于并发请求的限制手写
-const urls = ['url1', 'url2', 'url3', 'url4', 'url5'];
-const maxConcurrency = 3; // 设置最大并发数为3
-
-// 将urls数组按照maxConcurrency分组
-const groups = urls.reduce((acc, url, index) => {
-  const groupIndex = index % maxConcurrency;
-  if (!acc[groupIndex]) {
-    acc[groupIndex] = [];
-  }
-  acc[groupIndex].push(url);
-  return acc;
-}, []);
-
-// 执行每个分组中的请求，并返回结果数组
-const results = await Promise.all(
-  groups.map(group => Promise.all(group.map(url => fetch(url))))
-);
-
-// 将结果数组合并成一个数组
-const combinedResults = results.flat();
